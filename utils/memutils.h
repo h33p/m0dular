801a9c921b9054dd69976a9567c33a4feb8fee37
Aurimas Blažulionis <0x60@pm.me>
2018-08-10 16:27:31 +0300

    Add support for 32-bit dereference operation, add a page-by-page pattern scanning method.

--- utils/memutils.h
+++ utils/memutils.h
@@ -3,6 +3,7 @@
 
 #include "pattern_scan.h"
 #include "handles.h"
+#include <string.h>
 
 //External programs might want to use custom RPM/WPM functions
 #ifndef MEMUTILS_CUSTOM_RW
@@ -13,11 +14,35 @@ inline T Read(N addr)
 }
 
 template<typename T, typename N>
+inline void ReadArr(N addr, T* arr, size_t count)
+{
+	memcpy((void*)arr, *(void**)addr, sizeof(T) * count);
+}
+
+template<typename T, typename N>
 inline void Write(N addr, T value)
 {
 	return *(T*)addr = value;
 }
 
+template<typename T, typename N>
+inline void WriteArr(N addr, T* arr, size_t count)
+{
+	memcpy((void*)addr, (void*)arr, sizeof(T) * count);
+}
+
+#else
+template<typename T, typename N>
+T Read(N addr);
+
+template<typename T, typename N>
+void ReadArr(N addr, T* arr, size_t count);
+
+template<typename T, typename N>
+T Write(N addr, T value);
+
+template<typename T, typename N>
+void WriteArr(N addr, T* arr, size_t count);
 #endif
 
 inline uintptr_t GetAbsoluteAddress(uintptr_t addr, intptr_t offset, intptr_t instructionSize)
